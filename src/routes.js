c5618ec2f98d817406bd72f4c73c2c4a2ebeb692
Sippakorn Raksakiart <spkrsk.37@gmail.com>
2018-02-18 13:08:55 +0700

    add register route

--- src/routes.js
+++ src/routes.js
@@ -2,5 +2,6 @@ const CONFIG = require("./config")
 const userController = require("./controllers/userController")
 
 module.exports = (server) => {
-	server.post(`${CONFIG.URL_PREFIX}/login`, userController.login)
+  server.post(`${CONFIG.URL_PREFIX}/login`, userController.login)
+  server.post(`${CONFIG.URL_PREFIX}/register`, userController.register)
 };
