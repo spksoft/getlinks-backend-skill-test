a256f7febc0af68601207b33784beb157e1b09b7
Sippakorn Raksakiart <spkrsk.37@gmail.com>
2018-02-18 13:08:45 +0700

    update config.js : sync from .env

--- src/config.js
+++ src/config.js
@@ -4,4 +4,7 @@ module.exports = {
   APP_NAME: process.env.APP_NAME || null,
   PORT: process.env.PORT || null,
   URL_PREFIX: process.env.URL_PREFIX || null,
+  ACCESSS_TOKEN_EXP: process.env.ACCESSS_TOKEN_EXP || null,
+  REFREST_TOKEN_EXP: process.env.REFREST_TOKEN_EXP || null,
+  KEYPHASE: process.env.KEYPHASE || null,
 }
